3dcd10e06eec64ede49592ffab7a7ed738cfd2b0
Fanfara1329 <t985978@gmail.com>
2022-02-13 16:04:10 +0300

    Реклама с картинкой

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -37,5 +37,41 @@ def im():
                     <p>Вот она какая, красная планета.</p>
                   </body>
                 </html>"""
+
+
+@app.route('/promotion_image')
+def pr_im():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='/style.css')}" />
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="static/MARS.png">
+                        <div class="alert alert-dark" role="alert">
+                            <h1 class="alert-heading">Человечество вырастает из детства.</h1>
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            <h1 class="alert-heading">Человечеству мала одна планета.</h1>
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            <h1 class="alert-heading">Мы сделаем обитаемыми безжизненные планеты.</h1>
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            <h1 class="alert-heading">И начнём с Марса!</h1>
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            <h1 class="alert-heading">Присоединяйся!</h1>
+                        </div>
+                      </body>
+                    </html>"""
+
 
 app.run(port=8080, host='127.0.0.1')
