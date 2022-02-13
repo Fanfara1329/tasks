e960d7837db5490be7045b0e89868e39beacae46
Fanfara1329 <t985978@gmail.com>
2022-02-13 14:56:17 +0300

    Commit

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def first():
+    return '<h1>Миссия Колонизация Марса</h1>'
 @app.route('/index')
 def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница',
-                           username=user)
+    return '<h1>И на Марсе будут яблони цвести!</h1>'
 
 
-app.run()
+app.run(port=8080, host='127.0.0.1')
